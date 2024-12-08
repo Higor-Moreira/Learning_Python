4d126739c9856d0df4b5a3eb2e465e16c07f3716
Higor Moreira <igore.m.oliveira@gmail.com>
2024-12-08 18:20:09 -0300

    Create print.py

---
+++ Livro_Automatize_Tarefas/print.py
@@ -0,0 +1,18 @@
+# argumentos da funcao print 
+
+"""
+por default o print adiciona uma quebra de linha ao fim da string.
+Porem, isso pode ser altearado usando o argumento end=''
+
+"""
+print('Hello', end=' ')
+print('World!')
+
+
+'''
+Também é possivel mudar o espaco adicionado por default entre 
+elementos dentro da funcao print. Usando o argumento sep=''
+'''
+
+print('feijao', 'arroz', 'cafe', 'carne', sep=' - ')
+
